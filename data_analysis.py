ab3e16c2709b69d05af4602b8b75013b88100e35
ppieczywek <pieczywek.piotr@gmail.com>
2021-09-16 21:27:36 +0200

    Procedures moved into mtr class

--- data_analysis.py
+++ data_analysis.py
@@ -1,45 +1,57 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.signal
-import numpy as np
 from mtr import mtr
 
-file_name = "./DATA/NANO_K_3.mtr"
+file_name = "./DATA/NANOCEL_KW.CHAL_900PPM_3.mtr"
 header_length = 19
 test_data = pd.read_csv(file_name, sep=",", skiprows=header_length, decimal=".")
 test_data = test_data[['Elongation', 'Force']]
 test_data = test_data.groupby(['Elongation'])['Force'].mean().reset_index()
 
+
 init_pos = mtr.get_init_point(test_data["Force"].values)
+break_point = mtr.get_break_point(test_data["Force"].values)
+limits = mtr.get_slopes_limits(test_data['Force'].values,
+                               test_data['Elongation'].values, init_pos)
+values = mtr.get_slopes_values(test_data['Force'].values,
+                               test_data['Elongation'].values,
+                               init_pos,
+                               limits)
 
-test_data = test_data.iloc[0:test_data['Force'].argmax(), :]
-test_data["dF"] = scipy.signal.savgol_filter(test_data["Force"].values, 25, 1,
-                                             deriv=1, delta=1.0, axis=- 1,
-                                             mode='interp', cval=0.0)
+print(values)
 
-test_data["dF"] = test_data["dF"] - test_data["dF"].min()
-test_data["dF"] = test_data["dF"] / test_data["dF"].max()
-test_data["Elongation"] = test_data["Elongation"] / test_data["Elongation"].max()
-
-half_window = 30
-
-slopes = np.zeros(len(test_data["dF"]))
-for i in range(half_window, len(test_data["dF"]) - half_window):
-    a = np.polyfit(test_data.Elongation[i-half_window:i+half_window],
-                   test_data.dF[i-half_window:i+half_window], 1)[0]
-    slopes[i] = a
-
-test_data["slopes"] = slopes
-test_data["selection"] = ((test_data["slopes"].abs() < 0.5*test_data["slopes"].max()).astype(int))
-
-split_points = np.where(abs(np.diff(test_data["selection"].values, n=1, axis=-1)) == 1)[0]+1
-groups = np.split(test_data["selection"].values, split_points, axis=0)
-
-limits = np.column_stack((np.concatenate(([0], split_points), axis=None),
-                          np.concatenate((split_points-1, [len(test_data["selection"].values)-1]), axis=None)))
-
-limits = limits[np.where(np.asarray(list(map(np.sum, groups))) > 0)[0], :]
-limits = limits[np.where((limits < init_pos).sum(axis=1) == 0)[0], :]
+#
+#
+# test_data = test_data.iloc[0:test_data['Force'].argmax(), :]
+# test_data["dF"] = scipy.signal.savgol_filter(test_data["Force"].values, 25, 1,
+#                                              deriv=1, delta=1.0, axis=- 1,
+#                                              mode='interp', cval=0.0)
+#
+# test_data["dF"] = (test_data["dF"] - test_data["dF"].min()) / (test_data["dF"].max()-test_data["dF"].min())
+# test_data["Elongation"] = test_data["Elongation"] / test_data["Elongation"].max()
+#
+# half_window = 30
+#
+# slopes = np.zeros(len(test_data["dF"]))
+# for i in range(half_window, len(test_data["dF"]) - half_window):
+#     a = np.polyfit(test_data.Elongation[i-half_window:i+half_window],
+#                    test_data.dF[i-half_window:i+half_window], 1)[0]
+#     slopes[i] = a
+#
+# slopes[range(len(slopes) - half_window, len(slopes))] = slopes[len(slopes) - (half_window+1)]
+# slopes[range(0, half_window)] = slopes[half_window]
+#
+# test_data["slopes"] = slopes
+# test_data["selection"] = ((test_data["slopes"].abs() < 0.5*test_data["slopes"].max()).astype(int))
+#
+# split_points = np.where(abs(np.diff(test_data["selection"].values, n=1, axis=-1)) == 1)[0]+1
+# groups = np.split(test_data["selection"].values, split_points, axis=0)
+#
+# limits = np.column_stack((np.concatenate(([0], split_points), axis=None),
+#                           np.concatenate((split_points-1, [len(test_data["selection"].values)-1]), axis=None)))
+#
+# limits = limits[np.where(np.asarray(list(map(np.sum, groups))) > 0)[0], :]
+# limits = limits[np.where((limits < init_pos).sum(axis=1) == 0)[0], :]
 
 
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
@@ -50,13 +62,19 @@ y = test_data["Force"][init_pos]
 ax1.plot(test_data["Elongation"], test_data["Force"], 'b-')
 ax1.plot(x, y, 'ro')
 
+x = test_data["Elongation"][break_point]
+y = test_data["Force"][break_point]
+
+ax1.plot(test_data["Elongation"], test_data["Force"], 'b-')
+ax1.plot(x, y, 'ko')
+
 for i in range(limits.shape[0]):
     ax1.plot(test_data["Elongation"][limits[i, 0]:limits[i, 1]],
              test_data["Force"][limits[i, 0]:limits[i, 1]], 'yo')
 
 
-ax2.plot(test_data['Elongation'], test_data['dF'], '-')
-ax3.plot(test_data['Elongation'], test_data['slopes'], '-')
+# ax2.plot(test_data['Elongation'], test_data['dF'], '-')
+# ax3.plot(test_data['Elongation'], test_data['slopes'], '-')
 plt.show()
 
 
--- mtr.py
+++ mtr.py
@@ -31,3 +31,56 @@ class mtr():
         init_pos = var_change.argmax() + np.floor(0.5*wnd_half_width)
 
         return init_pos
+
+    @staticmethod
+    def get_break_point(force, force_drop_thr=0.25):
+        break_point = np.where((abs(np.diff(force, n=1, axis=0)) > (0.25*force.max())) == 1)[0]
+        return break_point[0]
+
+    @staticmethod
+    def get_slopes_limits(force, displacement, init_pos):
+
+        max_force_loc = force.argmax()
+        force = force[0:max_force_loc]
+        displacement = displacement[0:max_force_loc]
+
+        force_df = signal.savgol_filter(force, 25, 1, deriv=1, delta=1.0, axis=- 1,
+                                        mode='interp', cval=0.0)
+
+        force_df = (force_df - force_df.min()) / (force_df.max() - force_df.min())
+        displacement = displacement / displacement.max()
+
+        half_window = 30
+        slopes = np.zeros(len(force_df))
+
+        for i in range(half_window, len(force_df) - half_window):
+            a = np.polyfit(displacement[i-half_window:i+half_window],
+                           force_df[i-half_window:i+half_window], 1)[0]
+            slopes[i] = a
+
+        slopes[range(len(slopes) - half_window, len(slopes))] = slopes[len(slopes) - (half_window+1)]
+        slopes[range(0, half_window)] = slopes[half_window]
+
+        selection = ((abs(slopes) < 0.5*slopes.max()).astype(int))
+
+        split_points = np.where(abs(np.diff(selection, n=1, axis=-1)) == 1)[0]+1
+        groups = np.split(selection, split_points, axis=0)
+
+        limits = np.column_stack((np.concatenate(([0], split_points), axis=None),
+                                  np.concatenate((split_points-1, [len(selection)-1]), axis=None)))
+
+        limits = limits[np.where(np.asarray(list(map(np.sum, groups))) > 0)[0], :]
+        limits = limits[np.where((limits < init_pos).sum(axis=1) == 0)[0], :]
+        return limits
+
+    def get_slopes_values(force, displacement, init_pos, limits):
+        force = force - force[int(init_pos)]
+        displacement = displacement - displacement[int(init_pos)]
+
+        slopes_values = []
+        for i in range(limits.shape[0]):
+            a = np.polyfit(displacement[limits[i, 0]:limits[i, 1]],
+                           force[limits[i, 0]:limits[i, 1]], 1)[0]
+            slopes_values.append(a)
+
+        return slopes_values
